0ff9bec01851f1d3d30ba3f8c18e6386d0ada5e5
sreelatha <sree.thippana@gmail.com>
2022-04-09 17:50:30 +0530

    added sop statements

--- basicmain/src/main/java/org/capgenemi/basicmain/BasicmainApplication.java
+++ basicmain/src/main/java/org/capgenemi/basicmain/BasicmainApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BasicmainApplication {
 
 	public static void main(String[] args) {
+		System.out.println("hello");
 		SpringApplication.run(BasicmainApplication.class, args);
 	}
 
