1a8a2d19640981ca0b7bebb98f377cefdb648acf
sreelatha <sree.thippana@gmail.com>
2022-04-09 17:25:17 +0530

    springboot test project

---
+++ basicmain/src/main/java/org/capgenemi/basicmain/BasicmainApplication.java
@@ -0,0 +1,13 @@
+package org.capgenemi.basicmain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicmainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicmainApplication.class, args);
+	}
+
+}
---
+++ basicmain/src/test/java/org/capgenemi/basicmain/BasicmainApplicationTests.java
@@ -0,0 +1,13 @@
+package org.capgenemi.basicmain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicmainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
